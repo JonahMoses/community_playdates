c3ed942487e032a7a5b35ab11ffa2d396eaa9fb5
Jonah <Jonahkmoses@gmail.com>
2014-01-28 16:09:22 -0700

    Fix for user login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 CommunityPlaydates::Application.routes.draw do
-  
+
+  get "/users/login" => "landing#index" # Don't Remove
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
                    controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
+  
   root :to => "landing#index"
 
 end
