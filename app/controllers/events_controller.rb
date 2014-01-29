4aaf52a9489b842954d9b8222f1d43b2fc1d79ac
Adam89 <Adamm1@aston.ac.uk>
2014-01-29 10:38:56 -0700

    events controller is creating events baby

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    fail
+    if @event.save
+      redirect_to @event, notice: "Event successfully created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description, :name, :location, :starts_at, )
+  end
 end
