a06fccd2b4c32d8c7cf846519a3cd9173d350525
Adam89 <Adamm1@aston.ac.uk>
2014-01-29 09:33:19 -0700

    creating a new event spec is failing

--- spec/features/create_new_event_spec.rb
+++ spec/features/create_new_event_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe "Event" do
+
   it "allows me to make a new event" do
     visit root_path
     login
     click_on "Create A New Playdate"
 
     expect(current_path).to eq(new_event_path)
-    expect(page).to have_content("Make a New Play Date")
+    expect(page).to have_content("Make a New PlayDate")
 
    fill_in "Name", with: "Football with kids"
    fill_in "Description", with: "x" * 26
@@ -21,19 +22,4 @@ describe "Event" do
    expect(page).to have_content("Football with kids")
  end
 
-  it "adds a default image if none is specified" do
-
-    visit root_path
-    login
-    click_on "Create A New Playdate"
-
-    fill_in "Name", with: "Football Kids"
-    fill_in "Description", with: "D" * 25
-    fill_in "Location", with: "gSchool"
-    select "2013", from: "event[starts_at(1i)]"
-    select "June", from: "event[starts_at(2i)]"
-    select "24",   from: "event[starts_at(3i)]"
-    click_on "Create Playdate"
-
-  end
 end
