a1674d479e7f2b1815fb06807e15e2107ee5e3b1
Adam89 <Adamm1@aston.ac.uk>
2014-01-30 23:05:39 -0700

    all tests are passing

--- spec/features/join_playdates_spec.rb
+++ spec/features/join_playdates_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe "join a playdate page" do
 
   it "is possible to join a playdate" do
-    Role.create(identity: "attendee")
     location = FactoryGirl.create(:location)
-    event = FactoryGirl.create(:event, location_id: location.id)
+    user = FactoryGirl.create(:user)
+    role = FactoryGirl.create(:role, identity: "creator")
+    event = Event.create(name: "Yoo", location_id: location.id)
+    FactoryGirl.create(:registration, user_id: user.id, role_id:role.id, event_id: event.id)
+
     login
+
+    FactoryGirl.create(:role, identity: "attendee")
+
     click_on "View All Playdates"
     within("#event_#{event.id}") do
       expect(page).to have_link("Join Playdate")
