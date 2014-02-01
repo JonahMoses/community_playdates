d7b76622b65578bfc9b1bf99e85aed7a39988b54
Ben Horne <benhorne44@gmail.com>
2014-02-01 13:06:37 -0700

    fixes css ids for specs

--- spec/features/create_new_event_spec.rb
+++ spec/features/create_new_event_spec.rb
@@ -14,11 +14,11 @@ describe "Event" do
     expect(page).to have_content("Make a New Playdate")
     fill_in "event_name", with: "Soccer with kids"
     fill_in "Description", with: "fun " * 10
-    fill_in "Venue", with: "gSchool"
-    fill_in "Street address", with: "1062 Delaware st"
-    fill_in "City", with: "Denver"
-    fill_in "State", with: "CO"
-    fill_in "Zipcode", with: "80204"
+    fill_in "venue-name", with: "gSchool"
+    fill_in "street-address", with: "1062 Delaware st"
+    fill_in "event-city", with: "Denver"
+    fill_in "event-state", with: "CO"
+    fill_in "event-zipcode", with: "80204"
     fill_in "event[date]", with: "2/28/2014"
     fill_in "event[start_time]", with: "9:00 AM"
     fill_in "event[end_time]", with: "9:30 AM"
