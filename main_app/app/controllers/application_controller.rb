934f4e6796dba9aa74bc3daa0da565abd1209ab5
Ben Horne <benhorne44@gmail.com>
2014-02-06 14:30:56 -0700

    successfully logs in user sets permissions for main app

--- main_app/app/controllers/application_controller.rb
+++ main_app/app/controllers/application_controller.rb
@@ -3,15 +3,45 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   # before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user_info
 
 private
+
+  def authenticate_user_info
+    if (cookies.signed[:provider] &&
+        cookies.signed[:uid] &&
+        cookies.signed[:name] &&
+        cookies.signed[:oauth_token] &&
+        cookies.signed[:oauth_expires_at] &&
+        cookies.signed[:email])
+      set_user
+    else
+      cookies.destroy
+      redirect_to "http://localhost:3002"
+    end
+  end
 
   def current_user
     @current_user ||= set_user
   end
 
   def set_user
-    User.find_by(uid: cookies.signed[:uid])
+    auth = {provider:         cookies.signed[:provider],
+            uid:              cookies.signed[:uid],
+            name:             cookies.signed[:name],
+            oauth_token:      cookies.signed[:oauth_token],
+            oauth_expires_at: cookies.signed[:oauth_expires_at],
+            email:            cookies.signed[:email]}
+    user = User.from_omniauth(auth)
+    if user.persisted?
+      user.find_avatar
+      flash.notice      = "Signed in!"
+      session[:user_id] = user.id
+      @current_user = user
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to "http://localhost:3002"
+    end
   end
 
   helper_method :current_user
--- main_app/app/controllers/dashboard_controller.rb
+++ main_app/app/controllers/dashboard_controller.rb
@@ -1,12 +1,11 @@
 class DashboardController < ApplicationController
 
   def index
-
-      fail
     if current_user
     else
       redirect_to "http://localhost:3002"
     end
+
   end
 
 end
