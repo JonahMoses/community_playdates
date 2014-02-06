377ace76fb8053f6c50c7cd7ee6c7ce6f08e7b19
Ben Horne <benhorne44@gmail.com>
2014-02-06 10:03:51 -0700

    successful login on auth client and redirect to main app

--- authorisation_client/config/initializers/omniauth.rb
+++ authorisation_client/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.full_host = "http://localhost:3000"
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']#, {:provider_ignores_state => true} 
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']#, {:provider_ignores_state => true}
 end
