fc54df235eb6652bece33e61a7bf15ac4376ed86
Adam89 <Adamm1@aston.ac.uk>
2014-02-05 20:30:40 -0700

    created authorisation app

---
+++ authorisation_client/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthorisationClient::Application.config.secret_key_base = 'fafa6e74dc24fe045374d20c3486cdea41503010fe7d068b11051778b5b1a0f568399926cc325174d8a6478ad62f603980d2e678d9c857f7391af0a6eaf41ae4'
