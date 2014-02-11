1776d7150479f3038d8a11c24f6cff684c176c19
Antony Siegert <agsiegert@hotmail.com>
2014-02-10 22:57:17 -0700

    added testing for user_client user and friend models

---
+++ user_client/test/models/friend_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class FriendTest < ActiveSupport::TestCase
+  def test_it_exists
+    assert Friend
+  end
+end
+
+# friend = Friend.new(
+#               name: "Kevin Morrison Powell"],
+#               id: "503528568",
+#               @facebook)
