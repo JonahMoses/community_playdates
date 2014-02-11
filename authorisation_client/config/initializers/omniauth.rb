5c2301c6cf5e0941083b522bd20c5f8d0f3c9483
Ben Horne <benhorne44@gmail.com>
2014-02-11 13:11:41 -0700

    more vps setup

--- authorisation_client/config/initializers/omniauth.rb
+++ authorisation_client/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '226856227502795', 'c30262ea2c0c950e6e92e7a295fe1b68'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
