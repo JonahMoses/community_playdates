d348b19cd9436c829f61e1dc8ea4658af0876088
Adam89 <Adamm1@aston.ac.uk>
2014-02-11 17:52:33 -0700

    testing user create sends json objects

---
+++ user_client/test/integration/user_comm_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UserCommTest < ActionDispatch::IntegrationTest
+
+  def setup
+    Rails.application.config.secret_token
+    VCR.eject_cassette
+    VCR.turn_off!
+  end
+
+  test "POST '/users' sends created users json objects" do
+
+    User.any_instance.stubs(:find_avatar).returns("Noo")
+    User.any_instance.stubs(:find_large_avatar).returns("Hoo")
+
+    post '/users', {id: 1, user: {
+      email: "agsiegert@hotmail.com",
+      oauth_expires_at: "2014-04-12 04:18:51",
+      provider: "facebook",
+      uid: "536753365",
+      oauth_token: "CAADOUxTaZBssBAEbIatemoZBC8",
+      name: "Antony Siegert"
+    }}
+
+    user_json = JSON.parse(response.body)
+
+    assert_equal 12, user_json.count
+    assert_equal "Antony Siegert", user_json["name"]
+    assert_equal "agsiegert@hotmail.com", user_json["email"]
+    assert_equal "facebook", user_json["provider"]
+    assert_equal "536753365", user_json["uid"]
+
+  end
+
+end
