40ba9c17a68bd34c104e372d13a87b449403d9b8
Ben Horne <benhorne44@gmail.com>
2014-02-12 09:45:55 -0700

    update reg client for basic auth

--- registration_client/test/controllers/registrations_controller_test.rb
+++ registration_client/test/controllers/registrations_controller_test.rb
@@ -5,7 +5,11 @@ class RegistrationsControllerTest < ActionController::TestCase
   def test_it_sends_created_registrations_as_json
     registration = Registration.create!(user_id: 1, role_id: 1, event_id: 1)
     registration.inspect
-    post :create, :format => :json, :input => registration
+    post :create, {user_id: registration.user_id,
+                   role_id: registration.role_id,
+                   event_id: registration.event_id,
+                   auth_id: "hello"}
+
     assert_response :success
   end
 
