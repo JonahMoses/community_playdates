2eb2e5f9e773930400f3d7f8f9130e9a3bbb4268
Ben Horne <benhorne44@gmail.com>
2014-02-12 23:10:50 -0700

    add redirect to landing page if logged in

--- authorisation_client/app/controllers/landing_controller.rb
+++ authorisation_client/app/controllers/landing_controller.rb
@@ -1,5 +1,11 @@
 class LandingController < ApplicationController
   def index
-
+    if cookies.signed[:user_id]
+      if Rails.env.development?
+        redirect_to "http://localhost:3000"
+      else
+        redirect_to "http://communityplaydates.com:3000"
+      end
+    end
   end
 end
